df3bc364b88330f2974db8124a601d7f08702719
Senthil Sekar <ssekar@jminsure.com>
2020-06-19 15:23:36 -0500

    new project created

---
+++ aspnet-mvc-tab-specific-session/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace aspnet_mvc_tab_specific_session
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ aspnet-mvc-tab-specific-session/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="aspnet_mvc_tab_specific_session.MvcApplication" Language="C#" %>
